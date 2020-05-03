9cf14dbc74014a4b85e56e5b9769e42126409b47
Manuel Gauto <mgauto@mgenterprises.org>
2020-05-02 23:03:55 -0400

    Initial Commit

---
+++ Menu/Menu.h
@@ -0,0 +1,19 @@
+//
+//  Menu.h
+//  Menu
+//
+//  Created by Manuel Gauto on 5/2/20.
+//  Copyright © 2020 PocketIDE. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Menu.
+FOUNDATION_EXPORT double MenuVersionNumber;
+
+//! Project version string for Menu.
+FOUNDATION_EXPORT const unsigned char MenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Menu/PublicHeader.h>
+
+
